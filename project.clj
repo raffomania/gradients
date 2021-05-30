c5434893d5759f05fea7cbe14512cc526b648186
Rafael Epplée <hello@rafa.ee>
2021-05-30 17:48:59 +0200

    update clojure/script

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject gradients "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "wallpaper generator"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.10.866"]
                  [reagent "0.8.0-alpha2"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [binaryage/devtools "0.9.9"]
